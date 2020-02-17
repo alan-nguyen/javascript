7e7a608398a9d89f8d792e178006137f8aac3a98
Alan Nguyen <tuan8101@gmail.com>
2020-02-17 16:20:11 -0700

    Create a list with template strings

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-// Number to string
-val = String(5);
-val = String(4 + 4);
+const name = "Alan";
+const age = 29;
+const job = "Wed Developer";
+const city = "Edmonton";
 
-// Bool to string
-val = String(true);
+let html;
 
-// Date to string
-val = String(new Date());
+// Without template strings (es5)
+html =
+  "<ul>" +
+  "<li>Name: " +
+  name +
+  "</li> " +
+  "<li>Age: " +
+  age +
+  "</li>" +
+  "<li>Job: " +
+  job +
+  "</li>" +
+  "<li>City: " +
+  city +
+  "</li>" +
+  "</ul>";
 
-// Array to string
-val = String([1, 2, 3, 4]);
+// With template strings (es6)
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+  </ul>
+`;
 
-// toString()
-val = (5).toString();
-val = true.toString();
-
-// String to number
-val = Number("5");
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number("hello");
-val = Number([1, 2, 3]);
-
-val = parseInt("100.30");
-val = parseFloat("100.30");
-
-// Output
-// console.log(val);
-// console.log(typeof val);
-// // console.log(val.length);
-// console.log(val.toFixed(2));
-
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
+document.body.innerHTML = html;
