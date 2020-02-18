915b474081f38ee3a42d3de0fb4f35c166733aaa
Alan Nguyen <tuan8101@gmail.com>
2020-02-17 19:31:54 -0700

    Loop through array

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-// FUNCTION DECLARATIONS
+// GENERAL LOOPS
+const cars = ["Tesla", "Ford", "Mercedes", "BMW"];
 
-function greet(firstName = "John", lastName = "Doe") {
-  return `Hello ${firstName} ${lastName}`;
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
 }
-
-// console.log(greet());
-
-// FUNCTION EXPRESSIONS
-
-const square = function(x = 3) {
-  return x * x;
-};
-
-// console.log(square());
-
-// IMMIDIATELEY INVOKABLE FUNCTION EXPRESSION - IIFEs
-
-(function(name = "Stranger") {
-  console.log("Hello " + name);
-})();
