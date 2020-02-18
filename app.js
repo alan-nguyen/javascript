365a3e7ae97357eb833e0db84033927c8f3d9a63
Alan Nguyen <tuan8101@gmail.com>
2020-02-17 19:40:06 -0700

    Loop with for in

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ for (let i = 0; i < cars.length; i++) {
 }
 
 // FOREACH
-cars.forEach(function(car) {
-  console.log(car);
+cars.forEach(function(car, index) {
+  console.log(`${index} : ${car}`);
 });
 
 // MAP
@@ -22,3 +22,13 @@ const ids = users.map(function(user) {
 });
 
 console.log(ids);
+
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 40
+};
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
