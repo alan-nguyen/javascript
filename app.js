ab7b31052fb36b29f2c8767f2b840453b983f17a
Alan Nguyen <tuan8101@gmail.com>
2020-02-17 19:32:55 -0700

    Loop using forEach

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const cars = ["Tesla", "Ford", "Mercedes", "BMW"];
 for (let i = 0; i < cars.length; i++) {
   console.log(cars[i]);
 }
+
+// FOREACH
+cars.forEach(function(car) {
+  console.log(car);
+});
