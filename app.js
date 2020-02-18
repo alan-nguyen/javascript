e5c71aad4dfd6b54a4ab07d9f223026a1aecec61
Alan Nguyen <tuan8101@gmail.com>
2020-02-17 19:13:55 -0700

    Create function declarations

--- app.js
+++ app.js
@@ -1,42 +1,7 @@
-const name = "Alan";
-const age = 29;
-const job = "Wed Developer";
-const city = "Edmonton";
+// FUNCTION DECLARATIONS
 
-let html;
-
-// Without template strings (es5)
-html =
-  "<ul>" +
-  "<li>Name: " +
-  name +
-  "</li> " +
-  "<li>Age: " +
-  age +
-  "</li>" +
-  "<li>Job: " +
-  job +
-  "</li>" +
-  "<li>City: " +
-  city +
-  "</li>" +
-  "</ul>";
-
-function hello() {
-  return "hello";
+function greet(firstName = "John", lastName = "Doe") {
+  return `Hello ${firstName} ${lastName}`;
 }
 
-// With template strings (es6)
-html = `
-  <ul>
-    <li>Name: ${name}</li>
-    <li>Age: ${age}</li>
-    <li>Job: ${job}</li>
-    <li>City: ${city}</li>
-    <li>${2 + 2}</li>
-    <li>${hello()}</li>
-    <li>${age > 30 ? "Over 30" : "Under 30"}</li>
-  </ul>
-`;
-
-document.body.innerHTML = html;
+console.log(greet());
