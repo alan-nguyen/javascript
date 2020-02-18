cd15a0c102ef3e7f7e6fdb375102c4baacea0f8e
Alan Nguyen <tuan8101@gmail.com>
2020-02-17 19:16:14 -0700

    Create function expressions

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ function greet(firstName = "John", lastName = "Doe") {
   return `Hello ${firstName} ${lastName}`;
 }
 
-console.log(greet());
+// console.log(greet());
+
+// FUNCTION EXPRESSIONS
+
+const square = function(x = 3) {
+  return x * x;
+};
+
+console.log(square());
