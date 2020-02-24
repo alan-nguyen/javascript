047fb403b3d6513897c13ed7a647429ae27cb280
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 19:08:36 -0700

    Create personPrototypes

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-// Person constructor
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
-
-// Greeting
-Person.prototype.greeting = function() {
-  return `Hello there, ${this.firstName} ${this.lastName}`;
+const personPrototypes = {
+  greeting: function() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
 };
 
-const person1 = new Person("John", "Doe");
+const mary = Object.create(personPrototypes);
+mary.firstName = "Mary";
+mary.lastName = "Williams";
+mary.age = 30;
 
-// console.log(person1.greeting());
-
-// Customer constructor
-function Customer(firstName, lastName, phone, membership) {
-  Person.call(this, firstName, lastName);
-
-  this.phone = phone;
-  this.membership = membership;
-}
-
-// Inherit the Person protype methods
-Customer.prototype = Object.create(Person.prototype);
-
-// Make customer.protype return Customer
-Customer.prototype.constructor = Customer;
-
-// Create Customer
-const customer1 = new Customer("Tom", "Smith", "555-555-5555", "Standard");
-
-console.log(customer1);
+console.log(mary.greeting());
+console.log(mary);
