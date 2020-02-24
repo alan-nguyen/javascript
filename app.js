84998c11f96568d973c5b51872cc27d06836480a
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 18:55:25 -0700

    Create Person constructor

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 // Person constructor
-function Person(name, dob) {
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  this.calculateAge = function() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
 }
 
-// const alan = new Person("Alan", 29);
-// const Aaden = new Person("Aaden", 4);
-
-const alan = new Person("Alan", "7-8-1990");
-console.log(alan.calculateAge());
+// Greeting
+Person.prototype.greeting = function() {
+  return `Hello there, ${this.firstName} ${this.lastName}`;
+};
