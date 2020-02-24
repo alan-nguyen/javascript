225f1bcc34b0cac2a16142b1e63881a6f0df8803
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 19:16:19 -0700

    Create a new object

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const personPrototypes = {
   greeting: function() {
     return `Hello there ${this.firstName} ${this.lastName}`;
+  },
+  getsMarried: function(newLastName) {
+    this.lastName = newLastName;
   }
 };
 
@@ -9,5 +12,16 @@ mary.firstName = "Mary";
 mary.lastName = "Williams";
 mary.age = 30;
 
+mary.getsMarried("Thompson");
+
 console.log(mary.greeting());
 console.log(mary);
+
+const alan = Object.create(personPrototypes, {
+  firstName: { value: "Alan" },
+  lastName: { value: "Nguyen" },
+  age: { value: 29 }
+});
+
+console.log(alan);
+console.log(alan.greeting());
