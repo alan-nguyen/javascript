d19c180f6bb8476e88c3a378129ae4733cafb750
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 18:30:07 -0700

    Add dob

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // Person constructor
-function Person(name, age) {
+function Person(name, dob) {
   this.name = name;
-  this.age = age;
+  // this.age = age;
+  this.birthday = new Date(dob);
 }
 
-const alan = new Person("Alan", 29);
-const Aaden = new Person("Aaden", 4);
+// const alan = new Person("Alan", 29);
+// const Aaden = new Person("Aaden", 4);
+
+const alan = new Person("Alan", "7-8-1990");
+console.log(alan);
