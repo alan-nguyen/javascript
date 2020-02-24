4695f5235f5e3b284b73990065520d34913fee4c
Alan Nguyen <tuan8101@gmail.com>
2020-02-23 18:32:35 -0700

    Add calculate age

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ function Person(name, dob) {
   this.name = name;
   // this.age = age;
   this.birthday = new Date(dob);
+  this.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
 }
 
 // const alan = new Person("Alan", 29);
 // const Aaden = new Person("Aaden", 4);
 
 const alan = new Person("Alan", "7-8-1990");
-console.log(alan);
+console.log(alan.calculateAge());
