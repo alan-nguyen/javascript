ae66613a85559371f5585d055d23e9c30f960042
Alan Nguyen <tuan8101@gmail.com>
2020-02-24 19:36:14 -0700

    Create onprogress

--- app.js
+++ app.js
@@ -7,19 +7,25 @@ function loadData() {
   // OPEN
   xhr.open("GET", "data.txt", true);
 
-  console.log("READYSTATE", xhr.readyState);
+  // console.log("READYSTATE", xhr.readyState);
 
-  // xhr.onload = function() {
-  //   if (this.status === 200) {
-  //     console.log(this.responseText);
-  //   }
-  // };
+  // Optional - Used for spinners / loaders
+  xhr.onprogress = function() {
+    console.log("READYSTATE", xhr.readyState);
+  };
 
-  xhr.onreadystatechange = function() {
-    if (this.status === 200 && this.readyState === 4) {
+  xhr.onload = function() {
+    console.log("READYSTATE", xhr.readyState);
+    if (this.status === 200) {
       console.log(this.responseText);
     }
   };
+
+  // xhr.onreadystatechange = function() {
+  //   if (this.status === 200 && this.readyState === 4) {
+  //     console.log(this.responseText);
+  //   }
+  // };
 
   xhr.send();
 }
