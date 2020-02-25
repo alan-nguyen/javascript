0a984fdc2d36615d8ea77246ec8fada0220eb2b9
Alan Nguyen <tuan8101@gmail.com>
2020-02-24 19:54:30 -0700

    Output customer to HTML page

--- app.js
+++ app.js
@@ -7,7 +7,20 @@ function loadCustomer(e) {
 
   xhr.onload = function() {
     if (this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById("customer").innerHTML = output;
     }
   };
 
