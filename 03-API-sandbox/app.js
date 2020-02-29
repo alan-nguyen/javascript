908f409cb117e0c95bd4a2b4c6eec7cfa946216b
Alan Nguyen <tuan8101@gmail.com>
2020-02-28 21:53:38 -0700

    Create getExternal function

--- 03-API-sandbox/app.js
+++ 03-API-sandbox/app.js
@@ -2,6 +2,8 @@ document.getElementById("button1").addEventListener("click", getText);
 
 document.getElementById("button2").addEventListener("click", getJson);
 
+document.getElementById("button3").addEventListener("click", getExternal);
+
 // Get local text file data
 function getText() {
   fetch("test.txt")
@@ -28,6 +30,25 @@ function getJson() {
       let output = "";
       data.forEach(function(post) {
         output += `<li>${post.title}</h1>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get from external API
+function getExternal() {
+  fetch("https://api.github.com/users")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = "";
+      data.forEach(function(user) {
+        output += `<li>${user.login}</h1>`;
       });
       document.getElementById("output").innerHTML = output;
     })
