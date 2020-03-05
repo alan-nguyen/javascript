7877febd6c745459e85689d5565d489b90e689c0
Alan Nguyen <tuan8101@gmail.com>
2020-03-04 22:16:01 -0700

    Create EasyHTTP class

---
+++ 05-easyhttp2/easyhttp2.js
@@ -0,0 +1,14 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 2.0.0
+ * @author Alan Nguyen
+ * @license MIT
+ *
+ */
+
+class EasyHTTP {
+  // Make an HTTP GET Request
+  get() {}
+}
