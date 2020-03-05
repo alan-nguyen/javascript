719af3c3cb0b0958ed671f9a0d1b419e9f552046
Alan Nguyen <tuan8101@gmail.com>
2020-03-04 22:29:52 -0700

    Create User data

--- 05-easyhttp2/app.js
+++ 05-easyhttp2/app.js
@@ -1,7 +1,14 @@
 const http = new EasyHTTP();
 
 // Get Users
-http
-  .get("https://jsonplaceholder.typicode.com/users")
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// User Data
+const data = {
+  name: "John Doe",
+  user: "johndoe",
+  email: "jdoe@gmail.com"
+};
