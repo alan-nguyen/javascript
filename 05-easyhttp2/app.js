ca7fcf9e04884a0cb523095890737d70533d9c52
Alan Nguyen <tuan8101@gmail.com>
2020-03-04 22:24:40 -0700

    Update Get request with Promise

--- 05-easyhttp2/app.js
+++ 05-easyhttp2/app.js
@@ -2,3 +2,5 @@ const http = new EasyHTTP();
 
 // Get Users
 const users = http.get("https://jsonplaceholder.typicode.com/users");
+
+console.log(users);
--- 05-easyhttp2/easyhttp2.js
+++ 05-easyhttp2/easyhttp2.js
@@ -11,9 +11,11 @@
 class EasyHTTP {
   // Make an HTTP GET Request
   get(url) {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
   }
 }
