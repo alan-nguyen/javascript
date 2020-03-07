afe4e257a1de92bac7a6d0b523ec28c48e9356ca
Alan Nguyen <tuan8101@gmail.com>
2020-03-06 23:37:02 -0700

    Create easyhttp3

---
+++ 07-easyhttp3/easyhttp3.js
@@ -0,0 +1,69 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 3.0.0
+ * @author Alan Nguyen
+ * @license MIT
+ *
+ */
+
+class EasyHTTP {
+  // Make an HTTP GET Request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP POST Request
+  post(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP PUT Request
+  put(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP DELETE Request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve("Resource deleted"))
+        .catch(err => reject(err));
+    });
+  }
+}
