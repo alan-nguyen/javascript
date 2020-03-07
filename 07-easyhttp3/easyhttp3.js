7135082b465211eab3781f85b357575903183059
Alan Nguyen <tuan8101@gmail.com>
2020-03-06 23:43:30 -0700

    Create Post request with async

--- 07-easyhttp3/easyhttp3.js
+++ 07-easyhttp3/easyhttp3.js
@@ -17,19 +17,17 @@ class EasyHTTP {
   }
 
   // Make an HTTP POST Request
-  post(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
+  async post(url) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
     });
+
+    const resData = await response.json();
+    return resData;
   }
 
   // Make an HTTP PUT Request
