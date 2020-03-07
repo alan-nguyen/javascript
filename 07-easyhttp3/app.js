417ae6870e4d4cf599422ff0f2328066fe12de7d
Alan Nguyen <tuan8101@gmail.com>
2020-03-06 23:45:37 -0700

    Create Put request with async

--- 07-easyhttp3/app.js
+++ 07-easyhttp3/app.js
@@ -14,16 +14,16 @@ const data = {
 };
 
 // // Create User
-http
-  .post("https://jsonplaceholder.typicode.com/users", data)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
-// Update User
 // http
-//   .put("https://jsonplaceholder.typicode.com/users/2", data)
+//   .post("https://jsonplaceholder.typicode.com/users", data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
+
+// Update User
+http
+  .put("https://jsonplaceholder.typicode.com/users/2", data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
 // Delete User
 // http
--- 07-easyhttp3/easyhttp3.js
+++ 07-easyhttp3/easyhttp3.js
@@ -31,19 +31,17 @@ class EasyHTTP {
   }
 
   // Make an HTTP PUT Request
-  put(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
+  async put(url) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
     });
+
+    const resData = await response.json();
+    return resData;
   }
 
   // Make an HTTP DELETE Request
