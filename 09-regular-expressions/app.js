4d26549d3712f7fb243d983a204fae36f7833160
Alan Nguyen <tuan8101@gmail.com>
2020-03-18 22:10:58 -0600

    Add test function

--- 09-regular-expressions/app.js
+++ 09-regular-expressions/app.js
@@ -1,10 +1,13 @@
 let re;
 re = /hello/;
 
-console.log(re);
-console.log(re.source);
+// console.log(re);
+// console.log(re.source);
 
 // exec() - Return result in an array or null
-const result = re.exec('hello world');
+// const result = re.exec('hello world');
+
+// test() - Returns true or false
+const result = re.test('Hello');
 
 console.log(result);
